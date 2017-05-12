d4821492f205639b9a5f1d963729bd1f922f87b8
fern_vue@hotmail.com <fvue@blue.cs.sonoma.edu>
2017-05-11 17:00:41 -0700

    Finished creating project file -FV

--- Project2/dataCompress.py
+++ Project2/dataCompress.py
@@ -4,12 +4,13 @@
 
 import pygtrie as pygtrie
 import itertools
+import time
 
 def buildFact(w):
     trie = pygtrie.CharTrie()
     words = allSubstrings(w)
     for word in words:
-        trie[word] = True 
+        trie[word] = True
     return trie
 
 def allSubstrings(w):
@@ -24,29 +25,112 @@ def allSubstrings(w):
     return list(a)
 
 def allPossibleStrings(k):
+    if(k >20):
+        return["".join(seq) for seq in itertools.product("01", repeat=5)]
     return["".join(seq) for seq in itertools.product("01", repeat=k)]
+
+def getPrefixes(w):
+    pre = [w[0:len(w)-i] for i in range(0,len(w))]
+    return pre[::-1]
 
 def buildAD(trie, k):
     allPossible = []
-    for i in range (1,5):
+    count = 0
+    for i in range (1,k):
         possible = allPossibleStrings(i)
         for pos in possible:
             allPossible.append(pos)
     adTrie = pygtrie.CharTrie()
     for word in allPossible:
         if not trie.has_key(word):
-            possible = allSubstrings(word)
+            possible = getPrefixes(word)
+            flag = False
             for pos in possible:
-                if not adTrie.has_subtrie(pos):
-                    adTrie[word] = True
-                else:
-                    break;
+                if adTrie.has_key(pos):
+                    flag = True
+                    count = count + 1
+                    break
+                if (count > 1000000):
+                    break
+            if(not flag):
+                adTrie[word] = True
+        if (count > 1000000):
+            break
     return adTrie
 
+def getSuffixes(w):
+    return [w[i:] for i in range(1,len(w))]
+
+
 def encoder(AD, w):
-    v,r = ""
-    for char in w:
-        
+    v = ""
+    r = ""
+    for c in w:
+        suffixes = getSuffixes(v)
+        flag = False
+        for s in suffixes:
+            if(AD.has_key(s + "0") or (AD.has_key(s+"1"))):
+                flag = True
+        if(not flag):
+            r = r + c
+        v = v + c
+    return(len(w), r)
 
+def decoder(AD, w, n):
+    v = ""
+    count = 0
+    while(len(v) < n):
+        suffixes = getSuffixes(v)
+        flag = False
+        for s in suffixes:
+            if(AD.has_key(s + "0")):
+                v = v + "1"
+                flag = True
+                break
+            elif(AD.has_key(s+"1")):
+                v = v + "0"
+                flag = True
+                break
+        if (not flag):
+            b = w[count]
+            v = v + b
+            count = count + 1
+        flag = False
+    return v
 
-print buildAD(buildFact('10010') ,5)
+def encodeNdecode(w):
+
+    print "Input String:", w
+    print "Length of input string:", len(w)
+    buildTime = time.time()
+    AD = buildAD(buildFact(w),len(w))
+    afterBuild = time.time()
+    print "Time to build anti dictionary:",afterBuild - buildTime
+
+    encodeTime = time.time()
+    v,r = encoder(AD,w)
+    endTime = time.time()
+    print "Time to encode:", endTime - encodeTime
+    print "Encoded String:", r
+    print "Length of encoded string:", len(r)
+
+    decodeTime = time.time()
+    d = decoder(AD, r, v)
+    endTime = time.time()
+    print "Time to decode:", endTime - decodeTime
+    print "Decoded String:", d
+
+    if(w == d):
+        print "The decoded string and input string are equal"
+    else:
+        print "The decoded string and input string are NOT equal"
+    print
+
+w = '01001010'
+x = '01001101000'
+y = '1010011001010100001010100110010101000010101001100101010000101010011001010100001010100110010101000010'
+z='00100110010101010101100101110100110010101000010101001100101010000101010011001010100001010100110010101000010101001100101010000100001001100101010101011001011101001100101010000101010011001010100001010100110010101000010101001100101010000101010011001010100001000010011001010101010110010111010011001010100001010100110010101000010101001100101010000101010011001010100001010100110010101000010000100110010101010101100101110100110010101000010101001100101010000'
+encodeNdecode(w)
+encodeNdecode(x)
+encodeNdecode(y)
+encodeNdecode(z)
